e61234b287cdc43e4b1eb00a9ea30615e5171ece
Tanase Andrei Cristian <tanase.andrei.b5f@student.ucv.ro>
2026-05-28 23:12:57 +0300

    Added lightweight UI to display the stats without using an API client

---
+++ gateway/src/main/java/com/banking/gateway/controller/GeneratorProxyController.java
@@ -0,0 +1,38 @@
+package com.banking.gateway.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/generator")
+public class GeneratorProxyController {
+
+	@Value("${gateway.generator-url:http://banking-generator:8090}")
+	private String generatorUrl;
+
+	private final RestTemplate restTemplate;
+
+	public GeneratorProxyController(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	@PostMapping("/start")
+	public ResponseEntity<String> start(@RequestParam(defaultValue = "uniform") String scenario,
+										@RequestParam(defaultValue = "10") int rps,
+										@RequestParam(defaultValue = "60") int duration) {
+		String url = generatorUrl + "/generator/start?scenario=" + scenario + "&rps=" + rps + "&duration=" + duration;
+		return restTemplate.postForEntity(url, null, String.class);
+	}
+
+	@PostMapping("/stop")
+	public ResponseEntity<String> stop() {
+		return restTemplate.postForEntity(generatorUrl + "/generator/stop", null, String.class);
+	}
+
+	@GetMapping("/status")
+	public ResponseEntity<String> status() {
+		return restTemplate.getForEntity(generatorUrl + "/generator/status", String.class);
+	}
+}
